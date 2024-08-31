78a5507826e5db1178dd1b7536665467f982d23e
Marcus Nightingale <marcusnightingale.1@gmail.com>
2024-08-31 20:10:05 +0100

    Starting to rebuild the snackbar package

--- v1/demo/js/demo.js
+++
@@ -1,7 +0,0 @@
-$(document).on('click', '.snackbar', function () {
-    $.Snackbar('Hello World!', 'This is an example snackbar.', '', {
-        position: "snack-" + $(this).data('position'),
-        stack: false,
-        timeout: 6000
-    });
-});
